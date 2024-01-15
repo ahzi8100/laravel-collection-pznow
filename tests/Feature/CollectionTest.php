9f6f2a6074e510e7f137c5eefc72da175846ebd1
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 06:23:21 +0700

    Ordering Collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -374,4 +374,14 @@ class CollectionTest extends TestCase
             return $value == 8;
             }));
     }
+
+    public function testOrdering()
+    {
+        $collection = collect([1,4,2,3,5,6,8,7,9]);
+        $result = $collection->sort();
+        assertEqualsCanonicalizing([1,2,3,4,5,6,7,8,9], $result->all());
+
+        $result = $collection->sortDesc();
+        self::assertEqualsCanonicalizing([9,8,7,6,5,4,3,2,1], $result->all());
+    }
 }
