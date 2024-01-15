0ee466627ae4c8ef8472475c8a4f1862182ac4dd
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 02:58:26 +0700

    ForEach Collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -11,4 +11,12 @@ class CollectionTest extends TestCase
         $collection = collect([1,2,3]);
         self::assertEqualsCanonicalizing([1,2,3], $collection->all());
     }
+
+    public function testForEach()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        foreach ($collection as $key => $value) {
+            self::assertEquals($key+1, $value);
+        }
+    }
 }
