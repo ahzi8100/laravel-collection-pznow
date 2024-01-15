63a57b4e611c56231626854d833545efa0de9736
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-16 05:47:45 +0700

    Slicing Collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -275,4 +275,14 @@ class CollectionTest extends TestCase
             return $value["department"];
         })->all());
     }
+
+    public function testSlice()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        $result = $collection->slice(3);
+        assertEqualsCanonicalizing([4,5,6,7,8,9], $result->all());
+
+        $result = $collection->slice(3,2);
+        assertEqualsCanonicalizing([4,5], $result->all());
+    }
 }
